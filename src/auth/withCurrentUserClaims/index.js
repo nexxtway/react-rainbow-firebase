d8f196a4633b7aa220f697127a8efb0cebcc1b15
LeandroTorresSicilia <jtorressicilia@gmail.com>
2019-06-13 18:40:11 -0400

    feat: implement withCurrentUserClaims

---
+++ src/auth/withCurrentUserClaims/index.js
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react';
+import FirebaseApp from '../../firebase';
+
+export default function withCurrentUserClaims(WrappedComponent) {
+    return class extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                isLoading: true,
+                claims: {},
+                error: null,
+            };
+        }
+
+        componentDidMount() {
+            return FirebaseApp.instance
+                .auth()
+                .currentUser
+                .getIdTokenResult()
+                .then(idTokenResult => {
+                    this.setState({
+                        isLoading: false,
+                        claims: idTokenResult.claims,
+                    });
+                })
+                .catch(error => this.setState({
+                    isLoading: false,
+                    error,
+                }));
+        }
+
+        render() {
+            return (
+                <WrappedComponent
+                    {...this.props}
+                    {...this.state}
+                />
+            );
+        }
+    };
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import FirebaseApp from './firebase';
 import FSCollection from './firestore/FSCollection';
 import FSLookup from './firestore/FSLookup';
 import FSDoc from './firestore/FSDoc';
+import withCurrentUserClaims from './auth/withCurrentUserClaims';
 
 export {
     FirebaseApp,
     FSCollection,
     FSLookup,
     FSDoc,
+    withCurrentUserClaims,
 };
