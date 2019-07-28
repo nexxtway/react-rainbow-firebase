4b6b6bfa03265b82a5e1dc8cddd9ce00a80ed13b
LeandroTorresSicilia <jtorressicilia@gmail.com>
2019-07-27 21:03:47 -0400

    fix: FSCollection getOperator helper

--- src/firestore/FSCollection/helpers/get-operator.js
+++ src/firestore/FSCollection/helpers/get-operator.js
@@ -1,5 +1,5 @@
 const OPERATORS = ['eq', 'gt', 'gte', 'lt', 'lte', 'contains'];
 
 export default function getOperator(queryItem) {
-    return OPERATORS.find(operatorString => queryItem[operatorString]);
+    return OPERATORS.find(operatorString => queryItem[operatorString] !== undefined);
 }
