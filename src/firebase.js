e2a7a030e3752d18e78e38a25f1b8c42ef5badfc
LeandroTorresSicilia <jtorressicilia@gmail.com>
2019-08-22 00:38:34 -0400

    feat: implement FirestoreListeners

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,14 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import FirestoreListeners from './firestore/firestore-listeners';
 
 class FirebaseApp {
     initializeApp(config) {
         this.instance = firebase.initializeApp(config);
+        this.instance.listeners = {
+            unsubcribeFromAll: FirestoreListeners.unsubcribeFromAll,
+        };
     }
 }
 
---
+++ src/firestore/firestore-listeners.js
@@ -0,0 +1,36 @@
+class FirestoreListeners {
+    constructor() {
+        this.listeners = new Map();
+    }
+
+    isListening({ at }) {
+        return this.listeners.has(at);
+    }
+
+    register({ id, unsubscribe }) {
+        this.listeners.set(id, unsubscribe);
+    }
+
+    unsubscribe({ from }) {
+        if (this.listeners.has(from)) {
+            const unsubscribeListener = this.listeners.get(from);
+            if (typeof unsubscribeListener === 'function') {
+                unsubscribeListener();
+            }
+            this.listeners.delete(from);
+        }
+    }
+
+    unsubcribeFromAll() {
+        if (this.listeners.size > 0) {
+            this.listeners.forEach(unsubscribeListener => {
+                if (typeof unsubscribeListener === 'function') {
+                    unsubscribeListener();
+                }
+            });
+            this.listeners.clear();
+        }
+    }
+}
+
+export default new FirestoreListeners();
