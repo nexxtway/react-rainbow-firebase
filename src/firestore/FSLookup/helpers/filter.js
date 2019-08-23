710d4a2f4f92bc6c84b1cc4e308ae5e85e65ab10
LeandroTorresSicilia <jtorressicilia@gmail.com>
2019-08-23 19:23:48 -0400

    fix: filter function

--- src/firestore/FSLookup/helpers/filter.js
+++ src/firestore/FSLookup/helpers/filter.js
@@ -1,9 +1,25 @@
-export default function filter(query, options = []) {
+function getWords(query) {
+    return query
+        .split(/\s+/g)
+        .map(word => word.trim())
+        .filter(word => !!word);
+}
+
+function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+export default function filter(query, collection = []) {
     if (query) {
-        return options.filter(item => {
-            const regex = new RegExp(query, 'i');
-            return regex.test(item.label);
+        return collection.filter(item => {
+            const stringToMatch = item.label;
+            const words = getWords(query);
+
+            return words.every(word => {
+                const regex = new RegExp(escapeRegExp(word), 'i');
+                return regex.test(stringToMatch);
+            });
         });
     }
-    return [];
+    return collection;
 }
